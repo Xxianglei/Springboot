739c41625b27e4989f290638e83a32f96ca3927f
BB  zebra <1767925069@qq.com>
2019-04-13 21:58:39 +0800

    update pom.xml

--- src/main/java/com/xianglei/App.java
+++ src/main/java/com/xianglei/App.java
@@ -1,18 +1,1 @@
-package com.xianglei;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@EnableAutoConfiguration
-// 包含AOP的日志注解
-@MapperScan("com.xianglei.mapper")
-@ComponentScan("com.xianglei")
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-}
-
-
+package com.xianglei;import org.mybatis.spring.annotation.MapperScan;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.EnableAutoConfiguration;import org.springframework.context.annotation.ComponentScan;@EnableAutoConfiguration// 包含AOP的日志注解@MapperScan("com.xianglei.mapper")@ComponentScan("com.xianglei")public class App {    public static void main(String[] args) {        SpringApplication.run(App.class, args);    }}
