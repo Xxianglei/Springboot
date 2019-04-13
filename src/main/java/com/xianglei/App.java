1df0b4d975bff1054952104b40f325cad2b615ad
BB  zebra <1767925069@qq.com>
2019-04-13 20:40:30 +0800

    继承AOP日志统计 从此走向 idea

--- src/main/java/com/xianglei/App.java
+++ src/main/java/com/xianglei/App.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+// 包含AOP的日志注解
 @MapperScan("com.xianglei.mapper")
 @ComponentScan("com.xianglei")
 public class App {
